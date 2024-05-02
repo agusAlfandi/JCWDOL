bde253cd37b95fca78bff2acd3eec8e602efe76f
Agus <agusalfandi8@gmail.com>
2024-05-02 19:09:12 +0700

    fibonacci-fix

--- day2/day3.js
+++ day2/day3.js
@@ -1,3 +1,5 @@
+const { fi } = require("date-fns/locale");
+
 const result = (celcius) => {
   let Farenhait = (celcius * 9) / 5 + 32;
   return Farenhait;
@@ -51,37 +53,30 @@ const faktorial = (num) => {
   }
   return kali;
 };
-// console.log(faktorial(0));
-// // ============================================================
-// function fibonacci(n) {
-//   const fibSeq = [0, 1]; // Inisialisasi urutan dengan dua angka pertama
-
-//   for (let i = 2; i < n; i++) {
-//     const nextNum = fibSeq[i - 1] + fibSeq[i - 2];
-//     fibSeq.push(nextNum);
-//   }
-
-//   return fibSeq[n - 1]; // Mengembalikan angka ke-N dalam urutan Fibonacci
-// }
-
-// // Menampilkan angka pertama N dalam urutan Fibonacci dengan N = 15
-// const n = 15;
-// const fibonacciNumber = fibonacci(n);
-// console.log(
-//   `Angka pertama ${n} dalam urutan Fibonacci adalah: ${fibonacciNumber}`
-// );
-
-function fibonacciSequence(n) {
-  const fibSeq = [0, 1]; // Initialize sequence with first two numbers
-
-  for (let i = 2; i < n; i++) {
-    const nextNum = fibSeq[i - 1] + fibSeq[i - 2];
-    fibSeq.push(nextNum);
+console.log(faktorial(0));
+// ============================================================
+const fibonacci = (num) => {
+  let fib = [0, 1];
+  //          0, 1, 1, 2
+  for (let i = 2; i <= num; i++) {
+    let fibSum = (fib[i] = fib[i - 2] + fib[i - 1]);
+    console.log(fib[i]);
+    fib.push(fibSum);
   }
+  return fib;
+};
+console.log("🚀 ~ fibonacci ~ fibonacci:", fibonacci(15));
+// ============================================================
+// const fib2 = (N) => {
+//   let num1 = 0;
+//   let num2 = 1;
+//   let curr;
 
-  return fibSeq.slice(0, n).join(", "); // Return the first N Fibonacci numbers
-}
-
-const n = 15;
-const fibonacciNumbers = fibonacciSequence(n);
-console.log(`The first ${n} Fibonacci numbers are: ${fibonacciNumbers}`);
+//   for (let i = 2; i <= N; i++) {
+//     curr = num1 + num2;
+//     num1 = num2;
+//     num2 = curr;
+//   }
+//   return num2;
+// };
+// console.log("🚀 ~ fib2 ~ fib2:", fib2(15));
