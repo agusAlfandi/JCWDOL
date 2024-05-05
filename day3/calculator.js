8569388a98ff2a6645bbd4961ddf3e902fb2a83b
Agus <agusalfandi8@gmail.com>
2024-05-05 17:17:03 +0700

    day4: function dan array

---
+++ day3/calculator.js
@@ -0,0 +1,15 @@
+const Cal = (params1, params2, firtur) => {
+  if (firtur === "*") {
+    return params1 * params2;
+  } else if (firtur === "/") {
+    return params1 / params2;
+  } else if (firtur === "+") {
+    return params1 + params2;
+  } else if (firtur === "-") {
+    return params1 - params2;
+  } else {
+    return "error";
+  }
+};
+
+console.log(Cal(1, 2, "+"));
